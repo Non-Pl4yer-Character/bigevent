6ccebfbd9fb0a31d978e393f9e3ca935785d58ed
Non-Pl4yer-Character <1406793088@qq.com>
2020-07-30 16:04:31 +0800

    完成注册登录跳转

--- assets/js/login.js
+++ assets/js/login.js
@@ -14,34 +14,50 @@ $(function () {
         $register.hide()
     })
 
+    layui.form.verify({
+        pwd: [/^[\w]{6,10}$/, '密码只能由6-10位字母数字和下划线组成'],
+        // user: [/^[\S]{4,8}$/, '用户名只能4到8位且不能带有空格'],
+        repwd: function () {
+            if ($('#password1').val().trim() != $('#password2').val().trim()) {
+                return '两次密码输入不同'
+            }
+        }
+    })
 
+
+
+    $.ajaxPrefilter(function (option) {
+        option.url = 'http://ajax.frontend.itheima.net' + option.url
+    })
     // 注册功能请求
     $register.submit(function (e) {
         e.preventDefault()
-        const user = $('#username1').val().trim()
-        const pwd1 = $('#password1').val().trim()
-        const pwd2 = $('#password2').val().trim()
-        const reg1 = /^[\w]{6,10}$/
+        // const user = $('#username1').val().trim()
+        // const pwd1 = $('#password1').val().trim()
+        // const pwd2 = $('#password2').val().trim()
+        // const reg1 = /^[\w]{6,10}$/
 
 
-        if (!(reg1.test(user) && reg1.test(pwd1))) {
-            return $('#tips').html('用户名密码只能由6-10位字母数字和下划线组成')
-        }
-        if (pwd1 != pwd2) {
-            return $('#tips').html('两次密码输入不同')
-        }
+        // if (!(reg1.test(user) && reg1.test(pwd1))) {
+        //     return $('#tips').html('用户名密码只能由6-10位字母数字和下划线组成')
+        // }
+        // if (pwd1 != pwd2) {
+        //     return $('#tips').html('两次密码输入不同')
+        // }
 
         const data = $(this).serialize()
         $.ajax({
             method: 'post',
-            url: 'http://ajax.frontend.itheima.net/api/reguser',
+            url: '/api/reguser',
             data: data,
             success: function (res) {
                 if (res.status == 0) {
-                    alert('注册成功')
+                    layui.layer.msg('注册成功')
                     $tologin.click()
+                    $register[0].reset()
                 } else {
-                    $('#tips').html('用户名已被占用，请重新注册')
+                    // $('#tips').html('用户名已被占用，请重新注册')
+                    layui.layer.msg('用户名也被占用，请重新注册')
                 }
             }
         })
@@ -53,17 +69,18 @@ $(function () {
         e.preventDefault()
 
         const data = $(this).serialize()
-
-
         $.ajax({
             method: 'post',
-            url: 'http://ajax.frontend.itheima.net/api/login',
+            url: '/api/login',
             data: data,
             success: function (res) {
                 if (res.status == 1) {
-                    $('#tip').html('登录失败，用户名或密码错误')
+                    // $('#tip').html('登录失败，用户名或密码错误')
+                    layui.layer.msg('登录失败，用户名或密码错误')
                 } else {
-
+                    layui.layer.msg('登陆成功')
+                    localStorage.setItem('token', res.token)
+                    location.href = '/index.html'
                 }
                 console.log(res);
             }
